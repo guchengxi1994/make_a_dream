e8d1e8065d8235ab0a9c0207bf7aba83842e0e73
xiaoshuyui <528490652@qq.com>
2024-06-12 21:29:01 +0800

    upload resources

--- scripts/merge.py
+++ scripts/merge.py
@@ -32,7 +32,7 @@ def merge_images(images, direction='vertical', bg_color=(255,255,255)):
     return new_image
 
 # 修改路径
-image_paths = glob.glob("./resources/*.png")
+image_paths = glob.glob(r"D:\github_repo\resources\city/*.png")
 
 print(image_paths)
 
@@ -42,5 +42,7 @@ images = [Image.open(i) for i in image_paths]
 # 合并图像
 merged_image = merge_images(images, direction='horizontal',bg_color=None)
 
+merged_image = merged_image.resize((int(merged_image.size[0]/2),int(merged_image.size[1]/2)))
+
 # 保存合并后的图像
-merged_image.save('merged_image.png', format='PNG')
+merged_image.save('city.png', format='PNG')
