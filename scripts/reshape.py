475cfb1965f381808275080a5198fbcc81fdf4dc
xiaoshuyui <528490652@qq.com>
2024-06-13 21:20:08 +0800

    update map

---
+++ scripts/reshape.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import os
+
+def resize_images(directory, output_dir, size=(16, 16)):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for filename in os.listdir(directory):
+        if filename.endswith(('.png', '.jpg', '.jpeg')):
+            image_path = os.path.join(directory, filename)
+            with Image.open(image_path) as im:
+                im_resized = im.resize(size)
+                new_image_path = os.path.join(output_dir, filename)
+                im_resized.save(new_image_path)
+
+# 使用方法
+resize_images(r'D:\github_repo\resources\ground', r'D:\github_repo\resources\ground\16')
