993e98c9295c0cbe46cc2f34875fe6cf3d7215a5
xiaoshuyui <528490652@qq.com>
2024-06-18 21:03:28 +0800

    feat: add bgm

--- lib/game/components/page_wrapper.dart
+++ lib/game/components/page_wrapper.dart
@@ -1,19 +1,37 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'play_status/player_status_page.dart';
 
-class PageWrapper extends ConsumerWidget {
-  PageWrapper({super.key, required this.child});
+class PageWrapper extends ConsumerStatefulWidget {
+  const PageWrapper({super.key, required this.child});
   final Widget child;
 
+  @override
+  ConsumerState<PageWrapper> createState() => _PageWrapperState();
+}
+
+class _PageWrapperState extends ConsumerState<PageWrapper> {
   final _key = GlobalKey<ExpandableFabState>();
+  final player = AudioPlayer();
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await player.setReleaseMode(ReleaseMode.loop);
+      await player.play(AssetSource("audio/main_audio.mp3"),
+          mode: PlayerMode.lowLatency);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: child,
+      body: widget.child,
       floatingActionButtonLocation: ExpandableFab.location,
       floatingActionButton: ExpandableFab(
           key: _key,
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
 import isar_flutter_libs
 import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
